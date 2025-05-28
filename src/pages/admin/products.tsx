c4c726cf4b5bcdd9ce8bb009dc6a35b33d10a4c6
PoojaNarawad <poojanarawad16@gmail.com>
2025-05-28 14:40:45 +0530

    added products

--- src/pages/admin/products.tsx
+++ src/pages/admin/products.tsx
@@ -35,21 +35,20 @@ const columns: Column<DataType>[] = [
     accessor: "action",
   },
 ];
-
-const img =
-  "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2hvZXN8ZW58MHx8MHx8&w=1000&q=804";
-
+const img = "https://m.media-amazon.com/images/W/MEDIAX_1215821-T2/images/I/61cFB2OrkCL._SY355_.jpg";
 const img2 = "https://m.media-amazon.com/images/I/514T0SvwkHL._SL1500_.jpg";
+const img3 = "https://m.media-amazon.com/images/W/MEDIAX_1215821-T2/images/I/71ftBTXfsbL._AC_UL480_FMwebp_QL65_.jpg";
+const img4 = "https://m.media-amazon.com/images/I/71vFKBpKakL._SX522_.jpg"
 
 const arr: Array<DataType> = [
+ 
   {
     photo: <img src={img} alt="Shoes" />,
-    name: "Puma Shoes Air Jordan Cook Nigga 2023",
-    price: 690,
-    stock: 3,
-    action: <Link to="/admin/product/sajknaskd">Manage</Link>,
+    name: "eMeet S600 4K Webcam for Streaming",
+    price: 2166,
+    stock: 619,
+    action: <Link to="/admin/product/sdaskdnkasjdn">Manage</Link>,
   },
-
   {
     photo: <img src={img2} alt="Shoes" />,
     name: "Macbook",
@@ -58,19 +57,18 @@ const arr: Array<DataType> = [
     action: <Link to="/admin/product/sdaskdnkasjdn">Manage</Link>,
   },
   {
-    photo: <img src={img} alt="Shoes" />,
-    name: "Puma Shoes Air Jordan Cook Nigga 2023",
-    price: 690,
-    stock: 3,
+    photo: <img src={img3} alt="Shoes" />,
+    name: "Saregama Carvaan Premium Hindi - Portable Music Player",
+    price: 1365,
+    stock: 1119,
     action: <Link to="/admin/product/sajknaskd">Manage</Link>,
   },
-
   {
-    photo: <img src={img2} alt="Shoes" />,
-    name: "Macbook",
-    price: 232223,
-    stock: 213,
-    action: <Link to="/admin/product/sdaskdnkasjdn">Manage</Link>,
+    photo: <img src={img4} alt="Shoes" />,
+    name: "Fire TV Stick HD",
+    price: 4499,
+    stock: 444,
+    action: <Link to="/admin/product/sajknaskd">Manage</Link>,
   },
 ];
 
